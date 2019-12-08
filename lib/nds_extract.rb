09ceae960a5d0e630538d48d2b359d6726e6104c
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 06:37:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,10 +8,9 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
+  result = {}
   counter1 = 0
-  while counter1 < directors_database.length do
+  while counter1 < nds.length do
 	  counter2 = 0
 	  total = 0
     while counter2 < directors_database[counter1][movies].length do
@@ -30,4 +29,24 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   return result
+end
+
+def directors_totals(nds)
+  director_index = 0
+  totals = {}
+
+  while director_index < nds.length do
+    director_name = nds[director_index][:name]
+    totals[director_name] = 0
+    movie_index = 0
+
+    while movie_index < nds[director_index][:movies].length do
+      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+
+    director_index += 1
+  end
+
+  totals
 end
