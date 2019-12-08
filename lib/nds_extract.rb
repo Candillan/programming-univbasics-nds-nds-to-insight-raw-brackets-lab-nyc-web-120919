44a8b15eff25b871e92519f2d22d5642948ef168
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 06:43:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,12 +11,14 @@ def directors_totals(nds)
   result = {}
   counter1 = 0
   while counter1 < nds.length do
+	  name = nds[counter1][:name]
 	  counter2 = 0
-	  total[name] = 0
-    while counter2 < directors_database[counter1][movies].length do
-      total += directors_database[counter1][movies][counter2][3]
+	  result[name] = 0
+    while counter2 < nds[counter1][:movies].length do
+      result[name] += nds[counter1][:movies][counter2][:worldwide_gross]
+      counter2 += 1
     end
-    result[directors_database[counter1][name]] = total
+    counter1 += 1
 	end
 
   #
@@ -31,22 +33,3 @@ def directors_totals(nds)
   return result
 end
 
-def directors_totals(nds)
-  director_index = 0
-  totals = {}
-
-  while director_index < nds.length do
-    director_name = nds[director_index][:name]
-    totals[director_name] = 0
-    movie_index = 0
-
-    while movie_index < nds[director_index][:movies].length do
-      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1
-    end
-
-    director_index += 1
-  end
-
-  totals
-end
